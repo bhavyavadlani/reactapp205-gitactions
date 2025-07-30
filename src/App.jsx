24a3e73d29718215de9cf3a9eed61471f825548c
vadlanibhavya <vadlani.bhavya@gmail.com>
2025-07-30 16:36:25 +0530

    updated App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to My React Project - S205</h1>
+       <p>CI/CD and Cloud DevOps</p>
+      <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
+      <p>Done with Git Pages & Git Actions</p>
+    </div>
+  );
+}
+
+export default App;
